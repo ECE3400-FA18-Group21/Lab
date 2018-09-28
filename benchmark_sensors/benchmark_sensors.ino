86cc32f17f2add3a3dd232d9e185f7e5a9210a2f
Tyler Seiden Sherman <TylerSSherman98@gmail.com>
2018-09-28 17:26:43 -0400

    Add code for Optical Team Lab 2 demonstration

--- benchmark_sensors/benchmark_sensors.ino
+++ benchmark_sensors/benchmark_sensors.ino
@@ -1,6 +1,7 @@
 #include "mic.h"
+#include "ir_hat.h"
 /*
- * @date: 09.27.2018
+ * @date: 09.28.2018
  * @version: 01
  * @course: ECE 3400, Fall 2018
  * @team: 21
@@ -13,6 +14,8 @@ void setup() {
   fft_setup();
 }
 
+//IMPLEMENT SIMILAR CODE FOR IR DEMONSTRATION
+//ADD CODE FOR PUSHBOTTON FAIL SAFE (if microphone circuit fails)
 void loop() {
   int count = 0;
   Serial.println(detect_660hz());
