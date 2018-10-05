99ad57f94207506150220e5c8689a53a0cea4707
kwf37 <kwf37@cornell.edu>
2018-10-04 22:08:21 -0400

    Updated library files with lab 2 stuff

--- benchmark_sensors/ir_hat.cpp
+++ benchmark_sensors/ir_hat.cpp
@@ -1,63 +1,39 @@
-///*
-// * @date: 09.28.2018
-// * @version: 01
-// * @course: ECE 3400, Fall 2018
-// * @team: 21
-// */
-//#define LOG_OUT 1     // use the log output function
-//#define FFT_N 256     // set to 256 point fft
-//#define THRESHOLD 100 // bin thresholding for 6.08 kHz signal
-//
-//#include <FFT.h>
-//#include <Arduino.h>
-//#include "ir_hat.h"
-//
-//void fft_setup(){
-//  TIMSK0 = 0;          // turn off timer0 for lower jitter - delay() and millis() killed
-//  ADCSRA = 0xe5;       // set the adc to free running mode, set prescaler=32
-//  ADMUX = 0x40;        // use adc0
-//  DIDR0 = 0x01;        // turn off the digital input for adc0
-//}
-//
-///**
-// * [get_fft_bins] 
-// * Runs the FFT operation using the FFT library
-// * OUTPUT = FFT magnitude data in the form of a 128-bin byte array
-// */
-//byte * get_fft_bins(){
-//    cli();                                  // UDRE interrupt slows this way down on arduino1.0
-//    for (int i = 0 ; i < 512 ; i += 2) {    // save 256 samples
-//      while(!(ADCSRA & 0x10));              // wait for adc to be ready
-//      ADCSRA = 0xf5;                        // restart adc --> set prescaler=f5=32
-//      byte m = ADCL;                        // fetch adc data
-//      byte j = ADCH;
-//      int k = (j << 8) | m;                 // form into an int
-//      k -= 0x0200;                          // form into a signed int
-//      k <<= 6;                              // form into a 16b signed int
-//      fft_input[i] = k;                     // put real data into even bins
-//      fft_input[i+1] = 0;                   // set odd bins to 0
-//    }
-//    
-//    // window data, then reorder, then run, then take output
-//    fft_window();                           // window the data for better frequency response
-//    fft_reorder();                          // reorder the data before doing the fft
-//    fft_run();                              // process the data in the fft
-//    fft_mag_log();                          // take the output of the fft
-//    sei();                                  // turn interrupts back on
-//    return fft_log_out;
-//}
-//
-///**
-// * [detect_6008hz] 
-// * OUTPUT = returns TRUE if a 6008hz signal is detected by the Arduino,
-// *          & FALSE otherwise
-// * Uses THRESHOLD to determine whether a nearby robot is detected 
-// * (emitting IR at 6.08 kHz) or a decoy (emitting at 18 kHz)
-// */
-//bool detect_6008hz(){
-//    byte * fft_log_out = get_fft_bins();
-//    if (fft_log_out[41] > THRESHOLD && fft_log_out[42] > THRESHOLD)
-//        return true;
-//    else
-//        return false;
-//}
+/*
+ * @date: 09.28.2018
+ * @version: 01
+ * @course: ECE 3400, Fall 2018
+ * @team: 21
+ */
+#define IR_THRESHOLD 100 // bin thresholding for 6.08 kHz signal
+
+#include <Arduino.h>
+#include "ir_hat.h"
+#include "fft_util.h"
+
+void fft_setup_ir(){
+  fft_setup(0);
+}
+
+/**
+ * [get_fft_bins] 
+ * Runs the FFT operation using the FFT library
+ * OUTPUT = FFT magnitude data in the form of a 128-bin byte array
+ */
+byte * get_fft_bins_ir(){
+    return get_fft_bins(0);
+}
+
+/**
+ * [detect_6080hz] 
+ * OUTPUT = returns TRUE if a 6080hz signal is detected by the Arduino,
+ *          & FALSE otherwise
+ * Uses THRESHOLD to determine whether a nearby robot is detected 
+ * (emitting IR at 6.08 kHz) or a decoy (emitting at 18 kHz)
+ */
+bool detect_6080hz(){
+    byte * fft_log_out = get_fft_bins_ir();
+    if (fft_log_out[41] > IR_THRESHOLD && fft_log_out[42] > IR_THRESHOLD)
+        return true;
+    else
+        return false;
+}
--- benchmark_sensors/mic.cpp
+++ benchmark_sensors/mic.cpp
@@ -4,19 +4,14 @@
  * @course: ECE 3400, Fall 2018
  * @team: 21
  */
-#define LOG_OUT 1 // use the log output function
-#define FFT_N 256 // set to 256 point fft
-#define THRESHOLD 100 // bin thresholding for 660 Hz signal
+#define MIC_THRESHOLD 100 // bin thresholding for 660 Hz signal
 
-#include <FFT.h>
 #include <Arduino.h>
 #include "mic.h"
+#include "fft_util.h"
 
-void fft_setup(){
-  TIMSK0 = 0;          // turn off timer0 for lower jitter - delay() and millis() killed
-  ADCSRA = 0xe7;       // set the adc to free running mode, set prescaler=128 (default e5=32)
-  ADMUX = 0x45;        // use adc5
-  DIDR0 = 0x01;        // turn off the digital input for adc0
+void fft_setup_mic(){
+  fft_setup(1);
 }
 
 /**
@@ -24,27 +19,8 @@ void fft_setup(){
  * Runs the FFT operation using the FFT library
  * OUTPUT = FFT magnitude data in the form of a 128-bin byte array
  */
-byte * get_fft_bins(){
-    cli();                                  // UDRE interrupt slows this way down on arduino1.0
-    for (int i = 0 ; i < 512 ; i += 2) {    // save 256 samples
-      while(!(ADCSRA & 0x10));              // wait for adc to be ready
-      ADCSRA = 0xf7;                        // restart adc --> set prescaler=f7=128 (default f5=32)
-      byte m = ADCL;                        // fetch adc data
-      byte j = ADCH;
-      int k = (j << 8) | m;                 // form into an int
-      k -= 0x0200;                          // form into a signed int
-      k <<= 6;                              // form into a 16b signed int
-      fft_input[i] = k;                     // put real data into even bins
-      fft_input[i+1] = 0;                   // set odd bins to 0
-    }
-    
-    // window data, then reorder, then run, then take output
-    fft_window();                           // window the data for better frequency response
-    fft_reorder();                          // reorder the data before doing the fft
-    fft_run();                              // process the data in the fft
-    fft_mag_log();                          // take the output of the fft
-    sei();                                  // turn interrupts back on
-    return fft_log_out;
+byte * get_fft_bins_mic(){
+    return get_fft_bins(1);
 }
 
 /**
@@ -54,8 +30,8 @@ byte * get_fft_bins(){
  * Uses THRESHOLD to determine whether the tone is playing or not
  */
 bool detect_660hz(){
-    byte * fft_log_out = get_fft_bins();
-    if (fft_log_out[17] > THRESHOLD && fft_log_out[18] > THRESHOLD) //bins 18 & 19, but 0 based so subtract 1
+    byte * fft_log_out = get_fft_bins_mic();
+    if (fft_log_out[17] > MIC_THRESHOLD && fft_log_out[18] > MIC_THRESHOLD) //bins 18 & 19, but 0 based so subtract 1
         return true;
     else
         return false;
