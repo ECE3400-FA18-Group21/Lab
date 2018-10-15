c511709cb447bff4e3cbac4b5ca0c64b259f5450
Eric Lei <el536@cornell.edu>
2018-10-15 18:18:50 -0400

    added rf tx code

---
+++ benchmark_sensors/nordic_rf.h
@@ -0,0 +1,14 @@
+/*
+ * @date: 10.15.2018
+ * @version: 01
+ * @course: ECE 3400, Fall 2018
+ * @team: 21
+ * 
+ */
+
+
+/* Set up RF24 transceiver for transmit mode on Robot */
+void RF24_tx_setup();
+
+/* Send byte array of length n */
+void RF24_tx_send(byte *msg, int n);
