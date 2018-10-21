ad1b6a0fc0d6ab5eb113be314e189ebf5cd19b57
Eric Lei <el536@cornell.edu>
2018-10-21 18:54:55 -0400

    restore benchmark sensors

--- benchmark_sensors/maze.h
+++ benchmark_sensors/maze.h
@@ -1,9 +1,9 @@
 /*
-   @date: 09.28.2018
+   @date: 10.21.2018
    @version: 01
    @course: ECE 3400, Fall 2018
    @team: 21
-   Benchmark Sensors
+   Base Station Program - Lab3
 */
 #include <Arduino.h>
 
@@ -12,13 +12,14 @@
 */
 class Maze {
   private:
-    byte walls[]; // Wall map representation and visited intersections
     byte pos; //Robot position. First 4 bits are x coord, Second 4 bits are y coord
     byte heading; //Robot direction (can be 0-3)
     short visited[]; // Represents all visited nodes. Each entry in the array is a different
                      // Row (y coordinate) and the first 9 bits are whether the corresponding
                      // x coordinate on the grid was visited. Will have 9 entries, so whole
                      // array takes 18 bytes.
+                     
+    byte walls[324]; // Wall map representation and visited intersections
   public:
     Maze();
     /*
